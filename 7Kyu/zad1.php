e7b9a1227d41ecc5b82b662d3302233714c45122
GomaZkow <opypartur@gmail.com>
2024-10-10 13:07:27 +0300

    Первые делители

--- 7Kyu/zad1.php
+++ 7Kyu/zad1.php
@@ -1,20 +1,18 @@
 <?php
 
-function square_sum($numbers) : int {//принимаем числа в функции
-    $SumElements = 0;
-   
-   
-   
-   foreach($numbers as $elements){//При каждой итерации записываем слейд значение в перменную elem
-   
-   $elem_quadarat = POW($elements,2);//возводим значение в квадрат
-   
-   $SumElements +=$elem_quadarat;//Прибавляем числа в переменную
-   
-   }
-   
-   
-   return $SumElements;//возвращаем значение
-   
-   
-   }
+function multiples(int $m, float $n): array {
+ 
+    $index =0;//создаем переменную для индексации масива
+    
+    for($i=1;$i<=$m;$i++)//проходим циклом по числам
+    {
+      
+    $array[$index++] = (float)$n*$i;//записываем первые делители числа
+      
+    }
+    return $array;//возрващаем массив
+    
+    
+    
+    
+    }
